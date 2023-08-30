96ed2ea92573171cdceed241d4670ab1266e5c72
9iiota <crazyshocklight@gmail.com>
2023-08-30 17:09:48 +0200

    fsd

--- main.py
+++ main.py
@@ -10,11 +10,41 @@ import subprocess
 API_URL = "https://api-inference.huggingface.co/models/tarteel-ai/whisper-base-ar-quran"
 headers = {"Authorization": "Bearer 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"}
 
-def get_arabic_text(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
+AUDIO_CLIP_PATH = r"C:\Users\Crazy\Desktop\GitHub\quran\audio\_1.mp3"
+VIDEO_CLIP_PATH = r"C:\Users\Crazy\Desktop\GitHub\quran\clips\Koe no Katachi (95).mp4"
+
+def main():
+    # Get the Arabic text from the audio file
+    arabic_text = speech_to_text(AUDIO_CLIP_PATH)
+
+    # Get the verse key from the Arabic text
+    verse_key = get_verse_key(arabic_text["text"])["search"]["results"][0]["verse_key"]
+
+    # Get the tajweed of the verse
+    tajweed = get_tajweed(verse_key)
+
+    # Remove the verse number from the tajweed
+    last_space_index = tajweed.rfind(" ")  # Find the index of the last space
+    tajweed = tajweed[:last_space_index]  # Extract substring up to the last space
+
+    # Get the translation of the verse
+    translation = get_english_translation(verse_key)
+
+    # Possibly edit the translation
+    translation = edit_translation(translation)
+    print(f"Translation: `{translation}`")
+
+    # Create the video
+    create_video(AUDIO_CLIP_PATH, VIDEO_CLIP_PATH, tajweed, translation, "final.mp4")
+
+def speech_to_text(filename):
+    try:
+        with open(filename, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+        return response.json()
+    except Exception as e:
+        print(f"Error: {e}")
 
 def get_verse_key(text):
     response = requests.get(f"https://api.quran.com/api/v4/search?q={text}")
@@ -27,23 +57,14 @@ def get_tajweed(verse_key):
     clean_text = soup.get_text()
     return clean_text
 
-def get_translation(verse_key):
+def get_english_translation(verse_key):
     response = requests.get(f"https://api.quran.com/api/v4/quran/translations/20?verse_key={verse_key}")
     translation = response.json()["translations"][0]["text"]
     soup = BeautifulSoup(translation, "html.parser")
     clean_text = soup.get_text()
     return clean_text
 
-def main():
-    arabic_text = get_arabic_text(r"C:\Users\Crazy\Desktop\94\_1.mp3")
-    verse_key = get_verse_key(arabic_text["text"])["search"]["results"][0]["verse_key"]
-
-    tajweed = get_tajweed(verse_key)
-    last_space_index = tajweed.rfind(" ")  # Find the index of the last space
-    tajweed = tajweed[:last_space_index]  # Extract substring up to the last space
-
-    translation = get_translation(verse_key)
-
+def edit_translation(translation):
     # Create a temporary text file and open it with the default text editor
     with tempfile.NamedTemporaryFile(suffix=".txt", delete=False, mode="w", encoding="utf-8") as temp_file:
         temp_file.write(translation)
@@ -56,76 +77,75 @@ def main():
     with open(temp_file_path, "r", encoding="utf-8") as temp_file:
         translation = temp_file.read()
 
-    # Print the modified text
-    print(f"Translation: `{translation}`")
-
     # Clean up: Delete the temporary file
     os.remove(temp_file_path)
 
-    # Load the audio clip
-    audio_path = r"C:\Users\Crazy\Desktop\GitHub\quran\audio\_1.mp3"
-    audio_clip = AudioFileClip(audio_path)
+    return translation
 
-    # Create a shadow overlay clip
-    shadow_color = (0, 0, 0)  # Black color for the shadow
-    shadow_opacity = 0.5      # Opacity of the shadow (0 to 1)
-
-    # Load the video clip
-    video_path = r"C:\Users\Crazy\Desktop\GitHub\quran\video3.mp4"
-    video_clip = VideoFileClip(video_path)
-
-    # Ensure the video duration matches the audio duration
-    video_duration = audio_clip.duration
+def create_video(audio_clip_path, video_clip_path, tajweed, translation, output_path, shadow_opacity = 0.7, fade_duration = 0.5):
+    # Load the audio and video clip
+    audio_clip = AudioFileClip(audio_clip_path)
+    video_clip = VideoFileClip(video_clip_path)
 
     # Resize the video to match the audio duration
-    video_clip = video_clip.set_duration(video_duration)
+    video_clip = video_clip.set_duration(audio_clip.duration)
 
-    # Create a shadow overlay clip
-    shadow_clip = ColorClip(size=video_clip.size, color=shadow_color, duration=video_clip.duration)
+    # Crop the video to keep only the left side
+    cropped_video_clip = video_clip.crop(x1=0, x2=video_clip.h * .5625)
+
+    # Create a shadow overlay clip for the cropped video
+    shadow_color = (0, 0, 0)  # Black color for the shadow
+    shadow_opacity = 0.7      # Opacity of the shadow (0 to 1)
+
+    shadow_clip = ColorClip(size=cropped_video_clip.size, color=shadow_color, duration=cropped_video_clip.duration)
     shadow_clip = shadow_clip.set_opacity(shadow_opacity)
 
-    # Create a white text clip
-    top_text = f"{tajweed}"
-    top_txt_clip = TextClip(
+    # Create the top text
+    top_text = tajweed
+    top_text_clip = TextClip(
         top_text,
         fontsize=40,
         color='white',
-        size=video_clip.size,
+        size=cropped_video_clip.size,
         bg_color='transparent',  # Set background to transparent
         method='caption'
     )
 
+    # Create the bottom text
     bottom_text = f"\n\n\n\n{translation}"
-    bottom_txt_clip = TextClip(
+    bottom_text_clip = TextClip(
         bottom_text,
         fontsize=20,
         color='white',
-        size=video_clip.size,
+        size=cropped_video_clip.size,
         bg_color='transparent',  # Set background to transparent
         method='caption'
     )
 
     # Set the duration for the text overlay
-    top_txt_clip = top_txt_clip.set_duration(video_clip.duration)
-    bottom_txt_clip = bottom_txt_clip.set_duration(video_clip.duration)
+    text_duration = cropped_video_clip.duration
+
+    top_text_clip = top_text_clip.set_duration(text_duration)
+    bottom_text_clip = bottom_text_clip.set_duration(text_duration)
 
     # Apply fade-in and fade-out to the text
-    fade_duration = .5  # Duration of the fade effect in seconds
-    top_txt_clip = top_txt_clip.crossfadein(fade_duration).crossfadeout(fade_duration)
-    bottom_txt_clip = bottom_txt_clip.crossfadein(fade_duration).crossfadeout(fade_duration)
+    fade_duration = 0.5  # Duration of the fade effect in seconds
 
-    # Overlay the shadow and video clips
-    video_with_shadow = CompositeVideoClip([video_clip.set_position('center'), shadow_clip.set_position('center')])
+    top_text_clip = top_text_clip.crossfadein(fade_duration).crossfadeout(fade_duration)
+    bottom_text_clip = bottom_text_clip.crossfadein(fade_duration).crossfadeout(fade_duration)
 
-    # Overlay the text on the video with shadow
-    video_with_text_and_shadow = CompositeVideoClip([video_with_shadow, top_txt_clip.set_position('center')])
-    video_with_text_and_shadow = CompositeVideoClip([video_with_text_and_shadow, bottom_txt_clip.set_position('bottom')])
+    # Overlay the shadow over the cropped video clip
+    video_with_shadow = CompositeVideoClip([cropped_video_clip.set_position('center'), shadow_clip.set_position('center')])
+
+    # Overlay the text on the cropped video clip with shadow
+    video_with_text_and_shadow = CompositeVideoClip([video_with_shadow, top_text_clip.set_position('center')])
+    video_with_text_and_shadow = CompositeVideoClip([video_with_text_and_shadow, bottom_text_clip.set_position('bottom')])
 
     # Set the audio of the video
     video_with_audio = video_with_text_and_shadow.set_audio(audio_clip)
 
-    # Write the final video with shadow, text overlay, and audio
-    output_path = "output_video_with_all.mp4"
+    # Create the final video with shadow, text overlay, and audio
+    output_path = output_path
     video_with_audio.write_videofile(output_path, codec='libx264')
 
 if __name__ == "__main__":
