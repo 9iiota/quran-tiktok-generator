afb19169f0bcfaab196c849244156976641372f0
Burak Kilic <crazyshocklight@gmail.com>
2025-10-26 09:15:27 +0100

    Refactor VideoManager to include AudioController in constructor

--- app/video_manager.py
+++ app/video_manager.py
@@ -1,9 +1,16 @@
+from audio_controller import AudioController
 from file_controller import FileController
 
 
 class VideoManager:
-    def __init__(self, file_controller: FileController) -> None:
+    def __init__(
+        self, audio_controller: AudioController, file_controller: FileController
+    ) -> None:
+        self.audio_controller = audio_controller
         self.file_controller = file_controller
 
     def create_video(self):
         pass
+
+if __name__ == "__main__":
+    audio_controller = AudioController(
